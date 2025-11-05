3368bcad6c7695c916bedc31aa75862cf1ac1f70
yubin0404 <leeyubin0404@gmail.com>
2025-11-05 19:20:03 +0900

    plus_lexture_day2

---
+++ personal_practive/bubble_sorted.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void sort(int *arr, int n)
+{
+    int i, j, temp;
+    // 버블 정렬
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0 ; j < n - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1 ];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    printf("정렬된 수: ");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+}
+
+int main(void)
+{
+    int arr[5];
+
+    printf("숫자 입력 :");
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    sort(arr, 5);
+
+    return 0;
+    
+}
